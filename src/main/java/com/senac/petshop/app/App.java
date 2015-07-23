6d5d705a52fedc4a191349d983da3cdda60ec575
Rafael Lossurdo <lossurdo@gmail.com>
2015-07-23 18:56:14 -0300

    beta

--- src/main/java/com/senac/petshop/app/App.java
+++ src/main/java/com/senac/petshop/app/App.java
@@ -1,6 +1,7 @@
 package com.senac.petshop.app;
 
 import com.senac.petshop.util.Console;
+import com.senac.petshop.util.MenuConsole;
 
 /**
  *
@@ -8,11 +9,29 @@ import com.senac.petshop.util.Console;
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-        String v = Console.lerString("Diga seu nome:");
-        System.out.println(v);
-        
+        MenuConsole mc = new MenuConsole("PetShop4j Senac", 
+                "Sistema utilizado na disciplina de LAB2 do Senac (ADS)",
+                App.class);
+        mc.adicionarAcao("Cadastrar Dono", "cadastrarDono");
+        mc.adicionarAcao("Cadastrar Animal", "cadastrarAnimal");
+        mc.adicionarAcao("Vincular Dono aos seus Animais", "vincularDonoAnimal");
+        mc.adicionarAcao("Sair", "sair");
+
+        System.out.println(mc.getTexto());
+
+        Integer op = Console.lerInteger("Qual a sua opção:");
+
+        mc.executarAcao(op);
+               
     }
     
+    public void cadastrarDono() {
+        System.out.println("Método ok!");
+    }
+    
+    public void sair() {
+        System.exit(0);
+    }
 }
---
+++ src/main/java/com/senac/petshop/util/MenuConsole.java
@@ -0,0 +1,56 @@
+package com.senac.petshop.util;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+/**
+ *
+ * @author lossurdo
+ */
+public class MenuConsole {
+    private final String nomeSistema;
+    private final String descricao;
+    private final Class classe;
+    private HashMap<String, String> acaoTexto;
+    private HashMap<Integer, String> codigoAcao;
+    private Integer contador;
+
+    public MenuConsole(String nomeSistema, String descricao, Class classe) {
+        this.nomeSistema = nomeSistema;
+        this.descricao = descricao;
+        this.classe = classe;
+        this.contador = 1;
+        this.acaoTexto = new HashMap<>();
+        this.codigoAcao = new HashMap<>();
+    }
+
+    public void adicionarAcao(String texto, String acao) {
+        acaoTexto.put(acao, texto);
+        codigoAcao.put(contador, acao);
+        contador++;
+    }
+
+    public void executarAcao(Integer codigo) throws Exception {
+        String acao = codigoAcao.get(codigo);
+        Object obj = classe.newInstance();
+        Method m = classe.getMethod(acao, null);
+        System.out.println(">>> Executando método: " + acao);
+        m.invoke(obj, null);
+    }
+    
+    public String getTexto() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-- ").append(nomeSistema).append(" --\r\n");
+        sb.append(descricao).append("\r\n\r\n");
+        
+        for(Integer codigo : codigoAcao.keySet()) {
+            sb.append(codigo.toString()).append("-> ").
+                    append(acaoTexto.get(codigoAcao.get(codigo))).append("\r\n");
+        }
+        
+        sb.append("\r\n\r\n");
+        
+        return sb.toString();
+    }
+    
+}
